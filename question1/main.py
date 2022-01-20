a6473117ff818cc5aec6c68070113b530bb60817
hus2030 <98089092+hus2030@users.noreply.github.com>
2022-01-20 17:29:40 +0200

    Update main.py

--- question1/main.py
+++ question1/main.py
@@ -1,5 +1,5 @@
 from queries import view
-from create import create_tables
+from create import create_tables ,create_database
 from staging import staging_fill
 from populate import populate_tables
 import pyodbc
@@ -12,10 +12,12 @@ def main():
     conn = pyodbc.connect('Driver={SQL Server};'
                           'Server=' + config['DEFAULT']['Server']+
                           'Database=master;'
-                          'Trusted_Connection=yes;')
+                          'Trusted_Connection=yes;',autocommit=True)
     deposit_link = config['DEFAULT']['deposit_link']
     gameplay_link = config['DEFAULT']['gameplay_link']
 
+    print("Creating database question1...")
+    create_database(conn)
     print("Creating staging tables...")
     create_tables(conn)
     print("Populating staging tables...")
